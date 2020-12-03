13ba7e18fbd44b9d39952b94d5ff1bd0ba9ab6aa
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2020-12-03 18:36:55 +0100

    new commit

--- scripts/Ball.js
+++ scripts/Ball.js
@@ -1,26 +1,33 @@
 class Ball {
 
-    constructor(radius, mass) {
+    constructor(radius, mass, posX = 10, posY = 10, vX = 5, vY = 5) {
         this.radius = radius;
         this.mass = mass;
         this.v = {
-            x: 2,
-            y: 1
+            x: vX,
+            y: vY
         }
         this.pos = {
-            x: 20,
-            y: 20
+            x: posX,
+            y: posY
         }
-        console.log(`Ball with ${this.radius} radius, ${this.mass} created!`);
-        balls.add(this);
+        if ((this.pos.x - this.radius) <= 0 || (this.pos.x + this.radius) >= 600 ||
+        (this.pos.y - this.radius) <= 0 || (this.pos.y + this.radius) > 600) {
+            console.log("bad ball");
+        } else {
+            console.log(`ball created`);
+            balls.add(this);
+        }
     }
 
     checkWallCollision() {
-        if (this.pos.x < 0 || this.pos.x > 600) {
+        if ((this.pos.x - this.radius) <= 0 || (this.pos.x + this.radius) >= 600) {
+            console.log("boom wall");
             this.v.x *= -1;
         }
-        if (this.pos.y < 0 || this.pos.y > 600) {
+        if ((this.pos.y - this.radius) <= 0 || (this.pos.y + this.radius) > 600) {
             this.v.y *= -1;
+            console.log("boom wall");
         }
     }
 
@@ -28,7 +35,7 @@ class Ball {
 
         balls.forEach(ball => {
             if (this !== ball && this.isInCollisionArea(ball)) {
-                console.log("BOOM");
+                console.log("boom ball");
                 this.v.x *= -1;
                 this.v.y *= -1;
             }
@@ -37,7 +44,10 @@ class Ball {
     }
 
     isInCollisionArea(ball) {
-        return ((this.pos.x - ball.pos.x) ** 2 + (this.pos.y - ball.pos.y) ** 2) <= (this.radius + ball.radius);
+
+        return Math.sqrt((this.pos.x - ball.pos.x) ** 2 + (this.pos.y - ball.pos.y) ** 2)
+            < (this.radius + ball.radius);
+
     }
 
 }
@@ -45,18 +55,18 @@ class Ball {
 class BallsCollection {
 
     constructor() {
-        this.ballsCollection = [];
+        this.ballsList = [];
     }
 
     add(ball) {
-        this.ballsCollection.push(ball);
+        this.ballsList.push(ball);
     }
 
     draw() {
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        balls.ballsCollection.forEach(ball => {
+        balls.ballsList.forEach(ball => {
             ctx.beginPath();
             ctx.arc(ball.pos.x, ball.pos.y, ball.radius, 0, Math.PI*2);
             ctx.fillStyle = '#111111';
@@ -65,7 +75,7 @@ class BallsCollection {
             ball.pos.x += ball.v.x;
             ball.pos.y += ball.v.y;
             ball.checkWallCollision();
-            ball.checkBallCollision(balls.ballsCollection);
+            ball.checkBallCollision(balls.ballsList);
         });
     }
 
--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,13 +1,50 @@
 function addBall() {
 
     const radius = document.getElementById("radiusInput").value;
-    const mass = document.getElementById("massInput").value;
+    // const mass = document.getElementById("massInput").value;
 
-    const ball = new Ball(radius, mass);
+    let posX = Number.parseFloat(document.getElementById("xStartInput").value);
+    let posY = Number.parseFloat(document.getElementById("yStartInput").value);
 
-    const svg = document.getElementById("canvas");
-    svg.innerHTML += `<circle id='ball' cx='100', cy='100', r=${ball.radius}></circle>`;
+    const vX = Number.parseFloat(document.getElementById("vxStartInput").value);
+    const vY = Number.parseFloat(document.getElementById("vyStartInput").value);
 
+    new Ball(radius, 0, posX, posY, vX, vY);
+
+}
+
+function getMouseCoords(evt) {
+    console.log(`${evt.clientX} ${evt.clientY}`);
+}
+
+function addRandomBall() {
+
+    new Ball(
+        Math.random() * 40 + 10,
+        0,
+        Math.random() * 500 - 50,
+        Math.random() * 500 - 50,
+        Math.random() * 10 -5,
+        Math.random() * 10 -5
+    );
+
+}
+
+function randomizeV() {
+    balls.ballsList.forEach(ball => {
+        ball.v.x = (20 * Math.random()) - 10;
+        ball.v.y = (20 * Math.random()) - 10;
+    });
+}
+
+function showAddBall() {
+    document.getElementById("addBall").style.display = "block";
+    document.getElementById("randomizeV").style.display = "none";
+}
+
+function showRandomizeV() {
+    document.getElementById("addBall").style.display = "none";
+    document.getElementById("randomizeV").style.display = "block";
 }
 
 let balls = new BallsCollection();
