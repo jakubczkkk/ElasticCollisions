07a7add7822133719cbfc492baa88d9df6ad8e6e
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2020-12-20 13:47:52 +0100

    custom velocity implemented

--- scripts/Ball.js
+++ scripts/Ball.js
@@ -1,108 +1,112 @@
 class Ball {
 
-    constructor(radius, posX, posY, vX, vY) {
+  constructor(radius, posX, posY, vX, vY) {
 
-        this.radius = radius;
-        this.isPaused = false;
-        this.v = {
-            x: vX,
-            y: vY
-        }
-        this.pos = {
-            x: posX,
-            y: posY
-        }
-
-        if ((this.pos.x - this.radius) <= 0 || (this.pos.x + this.radius) >= 750 ||
-            (this.pos.y - this.radius) <= 0 || (this.pos.y + this.radius) > 600) {
-            console.log("bad ball");
-            addRandomBall();
-        } else {
-            console.log(`ball created`);
-            balls.add(this);
-        }
-
+    this.radius = radius;
+    this.isPaused = false;
+    this.v = {
+      x: vX,
+      y: vY
+    }
+    this.pos = {
+      x: posX,
+      y: posY
     }
 
-    checkWallCollision() {
-        if ((this.pos.x - this.radius) <= 0 || (this.pos.x + this.radius) >= 750) {
-            console.log("boom wall");
-            this.v.x *= -1;
-        }
-        if ((this.pos.y - this.radius) <= 0 || (this.pos.y + this.radius) > 600) {
-            this.v.y *= -1;
-            console.log("boom wall");
-        }
+    this.m = Math.PI * this.radius ** 2;
+
+    if ((this.pos.x - this.radius) <= 0 || (this.pos.x + this.radius) >= 750 ||
+      (this.pos.y - this.radius) <= 0 || (this.pos.y + this.radius) > 600) {
+      console.log("bad ball");
+      addRandomBall();
+    } else {
+      console.log(`ball created`);
+      balls.push(this);
     }
 
-    checkBallCollision(balls) {
+  }
 
-        balls.forEach(ball => {
-            if (this !== ball && this.isInCollisionArea(ball)) {
-                console.log("boom ball");
-                collision(this, ball);
-            }
-        });
-
+  wallCollision() {
+    if (this.pos.x - this.radius <= 0) {
+      this.pos.x = this.radius;
+      this.v.x *= -1;
+    } else if (this.pos.x + this.radius >= 750) {
+      this.pos.x = 750 - this.radius;
+      this.v.x *= -1;
+    } else if (this.pos.y - this.radius <= 0) {
+      this.pos.y = this.radius;
+      this.v.y *= -1;
+    } else if (this.pos.y + this.radius >= 600) {
+      this.pos.y = 600 - this.radius;
+      this.v.y *= -1;
     }
+  }
 
-    isInCollisionArea(ball) {
+  checkBallCollision(balls) {
 
-        return Math.sqrt((this.pos.x - ball.pos.x) ** 2 + (this.pos.y - ball.pos.y) ** 2)
-            <= (this.radius + ball.radius);
+    balls.forEach(ball => {
+      if (this !== ball && this.isInCollisionArea(ball)) {
+        console.log("boom ball");
+        collision(this, ball);
+      }
+    });
 
-    }
+  }
 
-}
+  isInCollisionArea(ball) {
 
-class BallsCollection {
+    return Math.sqrt((this.pos.x - ball.pos.x) ** 2 + (this.pos.y - ball.pos.y) ** 2)
+      <= (this.radius + ball.radius);
 
-    constructor() {
-        this.ballsList = [];
-    }
-
-    add(ball) {
-        this.ballsList.push(ball);
-    }
+  }
 
 }
 
 function collision(ball1, ball2) {
-    ball1.v.x *= -1;
-    ball1.v.y *= -1;
-    ball2.v.x *= -1;
-    ball2.v.y *= -1;
+  // ball1.v.x *= -1;
+  // ball1.v.y *= -1;
+  // ball2.v.x *= -1;
+  // ball2.v.y *= -1;
 }
 
 function draw() {
 
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    balls.ballsList.forEach(ball => {
+  balls.forEach(ball => {
 
-        ball.checkWallCollision();
-        ball.checkBallCollision(balls.ballsList);
-        if (!ball.isPaused) {
-            ball.pos.x += ball.v.x;
-            ball.pos.y += ball.v.y;
-        }
+    ball.wallCollision();
+    ball.checkBallCollision(balls);
 
-        ctx.beginPath();
-        ctx.arc(ball.pos.x, ball.pos.y, ball.radius, 0, Math.PI * 2);
-        ctx.fillStyle = '#222222';
-        ctx.fill();
-        ctx.closePath();
-
-    });
-
-    if (isCreatingNewBall) {
-        ctx.beginPath();
-        ctx.fillStyle = '#555555';
-        ctx.arc(newX, newY, radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
+    if (!ball.isPaused) {
+      ball.pos.x += ball.v.x;
+      ball.pos.y += ball.v.y;
     }
+
+    ctx.beginPath();
+    ctx.arc(ball.pos.x, ball.pos.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = '#222222';
+    ctx.fill();
+    ctx.closePath();
+
+  });
+
+  if (isCreatingNewBall) {
+    ctx.beginPath();
+    ctx.fillStyle = '#555555';
+    ctx.arc(newX, newY, radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  if (isSettingVelocity) {
+    ctx.beginPath();
+    ctx.moveTo(newX, newY);
+    ctx.lineTo(vxposition, vyposition);
+    ctx.stroke();
+    ctx.closePath();
+  }
 
 }
--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,165 +1,189 @@
 function addBall() {
 
-    isCreatingNewBall = true;
-    pauseBalls();
+  isCreatingNewBall = true;
+  pauseBalls();
 
-    newX = newY = 1000;
+  newX = newY = 1000;
 
-    document.getElementById("canvas").addEventListener(
-        "mousemove",
-        getMouseCoords,
-        true
-    );
+  document.getElementById("canvas").addEventListener(
+    "mousemove",
+    getMouseCoords
+  );
+
+  document.getElementById("canvas").addEventListener(
+    "click",
+    placeBall
+  );
+
+  document.getElementById("canvas").addEventListener(
+    "wheel",
+    setRadius
+  );
 
 }
 
 function getMouseCoords(e) {
 
-    const rect = document.getElementById("canvas").getBoundingClientRect();
-    newX = e.clientX - rect.left;
-    newY = e.clientY - rect.top;
-
-    document.getElementById("canvas").addEventListener(
-        "click",
-        placeBall,
-        true
-    );
-
-    document.getElementById("canvas").addEventListener(
-        "wheel",
-        setRadius,
-        true
-    );
+  const rect = document.getElementById("canvas").getBoundingClientRect();
+  newX = e.clientX - rect.left;
+  newY = e.clientY - rect.top;
 
 }
 
 function setRadius(e) {
 
-    e.preventDefault();
+  e.preventDefault();
 
-    radius -= e.deltaY * 0.01
+  radius -= e.deltaY * 0.01
 
-    if (radius < minRadius) {
-        radius = minRadius;
-    } else if (radius > maxRadius) {
-        radius = maxRadius;
-    }
+  if (radius < minRadius) {
+    radius = minRadius;
+  } else if (radius > maxRadius) {
+    radius = maxRadius;
+  }
 
-    document.getElementById("canvas").addEventListener(
-        "click",
-        createBall,
-        true
-    );
-
-}
-
-function createBall(e) {
-
-    new Ball(
-        radius,
-        newX,
-        newY,
-        2,
-        2
-    );
-
-    clean();
-
-}
-
-function clean() {
-
-    const canvas = document.getElementById("canvas");
-    canvas.removeEventListener("click", createBall, true);
-    canvas.removeEventListener("click", placeBall, true);
-    canvas.removeEventListener("mousemove", getMouseCoords, true);
-    canvas.removeEventListener("wheel", setRadius, true);
-
-    resumeBalls();
-    isCreatingNewBall = false;
+  document.getElementById("canvas").addEventListener(
+    "click",
+    placementReady
+  );
 
 }
 
 function placeBall(e) {
 
-    const rect = document.getElementById("canvas").getBoundingClientRect();
-    newX = e.clientX - rect.left;
-    newY = e.clientY - rect.top;
+  const canvas = document.getElementById("canvas");
+  canvas.removeEventListener("mousemove", getMouseCoords);
+  canvas.removeEventListener("wheel", setRadius);
+
+  isSettingVelocity = true;
+  canvas.addEventListener("click", placementReady);
+
+}
+
+function placementReady(e) {
+
+  const canvas = document.getElementById("canvas");
+  canvas.removeEventListener("mousemove", getMouseCoords);
+
+  document.getElementById("canvas").addEventListener("mousemove", setVelocity);
+  document.getElementById("canvas").addEventListener("click", createBall);
+
+}
+
+function setVelocity(e) {
+
+  const rect = document.getElementById("canvas").getBoundingClientRect();
+  vxposition = (e.clientX - rect.left);
+  vyposition = (e.clientY - rect.top);
+  vx = newX - vxposition;
+  vy = newY - vyposition;
+
+}
+
+function createBall(e) {
+
+  new Ball(
+    radius,
+    newX,
+    newY,
+    vx / 10,
+    vy / 10
+  );
+
+  clean();
+
+}
+
+function clean() {
+
+  const canvas = document.getElementById("canvas");
+  canvas.removeEventListener("mousemove", setVelocity);
+  canvas.removeEventListener("click", createBall);
+  canvas.removeEventListener("click", placeBall);
+  canvas.removeEventListener("click", placementReady);
+
+  resumeBalls();
+  isCreatingNewBall = false;
+  isSettingVelocity = false;
 
 }
 
 function addRandomBall() {
 
-    const randomRadius = Math.random() * (maxRadius - minRadius) + minRadius;
-    const canvas = document.getElementById("canvas");
-    const width = Math.random() * (750 - 2 * randomRadius) + randomRadius;
-    const height = Math.random() * (650 - 2 * randomRadius) + randomRadius;
+  const randomRadius = Math.random() * (maxRadius - minRadius) + minRadius;
+  const width = Math.random() * (750 - 2 * randomRadius) + randomRadius;
+  const height = Math.random() * (650 - 2 * randomRadius) + randomRadius;
 
-    if (balls.ballsList.length <= 15) {
-        new Ball(
-            randomRadius,
-            width,
-            height,
-            Math.random() * 10 - 5,
-            Math.random() * 10 - 5
-        );
-    } else {
-        alert("Too much ball on board;")
-    }
+  if (balls.ballsList.length <= 15) {
+    new Ball(
+      randomRadius,
+      width,
+      height,
+      Math.random() * 10 - 5,
+      Math.random() * 10 - 5
+    );
+  } else {
+    alert("Too much ball on board;")
+  }
 
 }
 
 function pauseBalls() {
 
-    balls.ballsList.forEach(ball => ball.isPaused = true);
+  balls.forEach(ball => ball.isPaused = true);
 
 }
 
 function resumeBalls() {
 
-    balls.ballsList.forEach(ball => ball.isPaused = false);
+  balls.forEach(ball => ball.isPaused = false);
 
 }
 
 function randomizeV() {
-    balls.ballsList.forEach(ball => {
-        ball.v.x = (20 * Math.random()) - 10;
-        ball.v.y = (20 * Math.random()) - 10;
-    });
+  balls.forEach(ball => {
+    ball.v.x = Math.random() * 10 - 5;
+    ball.v.y = Math.random() * 10 - 5;
+  });
 }
 
 function clearBoard() {
-    balls.ballsList = [];
+  balls = [];
 }
 
 function showOptions() {
 
-    document.getElementById("navButtonOptions").style.background = "#FFAD36";
-    document.getElementById("navButtonAbout").style.background = "white";
-    document.getElementById("menuDivOptions").style.display = "block";
-    document.getElementById("menuDivAbout").style.display = "none";
+  document.getElementById("navButtonOptions").style.background = "#FFAD36";
+  document.getElementById("navButtonAbout").style.background = "white";
+  document.getElementById("menuDivOptions").style.display = "block";
+  document.getElementById("menuDivAbout").style.display = "none";
 
 }
 
 function showAbout() {
 
-    document.getElementById("navButtonOptions").style.background = "white";
-    document.getElementById("navButtonAbout").style.background = "#FFAD36";
-    document.getElementById("menuDivOptions").style.display = "none";
-    document.getElementById("menuDivAbout").style.display = "block";
-    console.log(getEventListeners(window))
+  document.getElementById("navButtonOptions").style.background = "white";
+  document.getElementById("navButtonAbout").style.background = "#FFAD36";
+  document.getElementById("menuDivOptions").style.display = "none";
+  document.getElementById("menuDivAbout").style.display = "block";
+  console.log(getEventListeners(window))
 
 }
 
-let balls = new BallsCollection();
+let balls = [];
 let isCreatingNewBall = false;
+let isSettingVelocity = false;
 let newX;
 let newY;
+let vx;
+let vy;
+let vxposition;
+let vyposition;
 const minRadius = 10;
 const maxRadius = 60;
 const defaultRadius = 20;
 let radius = defaultRadius;
 
 window.onload = function () {
-    setInterval(draw, 10);
+  setInterval(draw, 10);
 };
